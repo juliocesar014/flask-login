657abd7443ed04f84b67c8c5942133a021ce973e
juliocesar014 <juliocesar014@live.com>
2023-03-02 15:26:37 -0300

    feat: Create register method

---
+++ accounts/forms.py
@@ -0,0 +1,25 @@
+from flask_wtf import FlaskForm
+from wtforms import EmailField, PasswordField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
+
+from accounts.models import User
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField("Email", validators=[DataRequired(), Email(message="Please enter a valid email address"), Length(max=255)])
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password", message="Passwords must match")])
+    
+    def validate(self):
+        initial_validation = super(RegisterForm, self).validate()
+        if not initial_validation:
+            return False
+        
+        user = User.query.filter_by(email=self.email.data).first()
+        if user:
+            self.email.errors.append("Email address already in use")
+            return False
+        
+        return True
+    
+    
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, redirect, url_for, flash, request
+from flask_login import current_user, login_user
+from accounts.forms import RegisterForm
+from src import db
+from accounts.models import User
 
 accounts_bp = Blueprint('accounts', __name__)
+
+
+@accounts_bp.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        flash("You are already logged in", "info")
+        return redirect(url_for("core.home"))
+    form = RegisterForm(request.form)
+    if form.validate.on_submit():
+        user = User(form.email.data, form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        
+        login_user(user)
+        flash("You have successfully registered and are now logged in. Welcome", "success")
+        
+        return redirect(url_for("core.home"))
+    
+    return render_template("accounts/register.html", form=form)
